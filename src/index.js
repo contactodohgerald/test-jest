ebaf759f13589a1fb1b5134b690b99296648cab3
Gerald Odoh <contactodohgerald@gmail>
2023-06-16 10:28:48 +0100

    made changes to the docker-compose file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/user', (req, res) => {
-    return res.status(200).send({
+    return res.status(200).json({
         status: true,
         message:'success'
     })
 })
 
-app.listen(5000, () => console.log('listening on port 5000'))
+app.listen(5050, () => console.log('listening on port 5050'))
